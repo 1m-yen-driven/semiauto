08b01c5e1eb8b6e9eaf8c4e9c55b6cb203712ee8
nakario <nakario.jp@gmail.com>
2021-09-11 15:07:11 +0900

    Add hostname setting

--- initial_settings.sh
+++ initial_settings.sh
@@ -4,13 +4,14 @@ set -eu
 
 . ./env.sh
 
-init_settings=$(mktemp)
-cat <<EOF >$init_settings
+for app in ${APP_NAMES[@]]}; do
+  init_settings=$(mktemp)
+  cat <<EOF >$init_settings
 sudo apt update
 sudo apt install -y git curl
 curl https://raw.githubusercontent.com/Muratam/dotfiles/isucon/init.sh | bash
+sudo hostnamectl set-hostname ${app}
 EOF
-for app in ${APP_NAMES[@]]}; do
   scp $init_settings $app:/tmp/init_settings
   ssh $app "bash /tmp/init_settings"
 done
